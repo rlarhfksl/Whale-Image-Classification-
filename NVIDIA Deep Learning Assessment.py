4bcf2ecef52e015c246a1e64aa158b3f4b3dd5e8
rlarhfksl <65189098+rlarhfksl@users.noreply.github.com>
2020-05-12 14:23:29 +0900

    Update NVIDIA Deep Learning Assessment.py

--- NVIDIA Deep Learning Assessment.py
+++ NVIDIA Deep Learning Assessment.py
@@ -39,8 +39,8 @@ import caffe
 import cv2
 import sys
 
-MODEL_JOB_DIR = '/dli/data/digits/20190926-221500-abfc'  ## Set this to be the job number for your model
-DATASET_JOB_DIR = '/dli/data/digits/20190926-220654-3c91'  ## Set this to be the job number for your dataset
+MODEL_JOB_DIR = '/dli/data/digits/20200512-033523-4e7f'  ## Set this to be the job number for your model
+DATASET_JOB_DIR = '/dli/data/digits/20200512-032938-6859'  ## Set this to be the job number for your dataset
 
 get_ipython().system(u'ls $MODEL_JOB_DIR')
 
@@ -75,7 +75,7 @@ def deploy(img_path):
     # Create an input that the network expects. 
     input_image = caffe.io.load_image(DATASET_JOB_DIR+'/mean.jpg')
     input_image = cv2.resize(input_image, (256,256))
-    mean_image = caffe.io.load_image('/dli/data/digits/20190926-220654-3c91/mean.jpg')
+    mean_image = caffe.io.load_image('/dli/data/digits/20200512-032938-6859/mean.jpg')
     ready_image = input_image-mean_image
     
 #spot for viz
